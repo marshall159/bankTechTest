b971fecb7fa6ed983756d958fd1a82d8fdf78304
Aneel Marshall <aneel.marshall@outlook.com>
2018-10-09 14:19:41 +0100

    refactor: tests to use before hook

--- account.test.js
+++ account.test.js
@@ -1,23 +1,28 @@
 import Account from './account';
 
+var account;
+
+beforeEach(() => {
+    account = new Account();
+});
+
 test('initial account balance 0', () => {
-    let account = new Account();
     expect(account.balance).toBe(0);
 });
 
-test('account opened with an initial deposit of £100', () => {
-    let account = new Account(100);
-    expect(account.balance).toBe(100);
-});
+describe('account opened with different initial deposit', () => {
+    test('initial deposit of £100', () => {
+        let account = new Account(100);
+        expect(account.balance).toBe(100);
+    });
+})
 
 test('make a deposit', () => {
-    let account = new Account();
     account.deposit(50)
     expect(account.balance).toBe(50);
 });
 
 test('make a deposit with a negative amount', () => {
-    let account = new Account();
     expect(function() {
         account.deposit(-50)
     }).toThrow();
