e9a6181e79952b7ae736675743bd8cdb5d96c378
Aneel Marshall <aneel.marshall@outlook.com>
2018-10-09 12:34:21 +0100

    test: account opening with deposit

--- account.test.js
+++ account.test.js
@@ -1,6 +1,11 @@
 import Account from './account';
 
 test('initial account balance 0', () => {
-    let account = new Account()
+    let account = new Account();
     expect(account.balance).toBe(0);
+});
+
+test('account opened with an initial deposit of £100', () => {
+    let account = new Account(100);
+    expect(account.balance).toBe(100);
 });
