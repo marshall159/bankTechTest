d49dd9735fd8384c43496ca42729bc2076f9d2fc
Aneel Marshall <aneel.marshall@outlook.com>
2018-10-09 14:53:50 +0100

    refactor: use describe blocks to organise tests

--- account.test.js
+++ account.test.js
@@ -6,37 +6,42 @@ beforeEach(() => {
     account = new Account();
 });
 
-test('initial account balance 0', () => {
-    expect(account.balance).toBe(0);
-});
+describe('initialise bank account', () => {
+    test('initial account balance 0', () => {
+        expect(account.balance).toBe(0);
+    });
 
-describe('account opened with different initial deposit', () => {
     test('initial deposit of £100', () => {
         let account = new Account(100);
         expect(account.balance).toBe(100);
     });
-})
-
-test('make a deposit', () => {
-    account.deposit(50)
-    expect(account.balance).toBe(50);
 });
 
-test('make a deposit with a negative amount', () => {
-    expect(function() {
-        account.deposit(-50)
-    }).toThrow();
+describe('deposit', () => {
+    test('make a deposit', () => {
+        account.deposit(50)
+        expect(account.balance).toBe(50);
+    });
+
+    test('make a deposit with a negative amount', () => {
+        expect(function() {
+            account.deposit(-50)
+        }).toThrow();
+    });
 });
 
-test('make a withdrawal', () => {
-    account.deposit(50);
-    account.withdraw(40);
-    expect(account.balance).toBe(10);
+describe('wihdraw', () => {
+    test('make a withdrawal', () => {
+        account.deposit(50);
+        account.withdraw(40);
+        expect(account.balance).toBe(10);
+    });
+    
+    test('withdraw more than account balance throws an error', () => {
+        account.deposit(50);
+        expect(function() {
+            account.withdraw(60)
+        }).toThrow();
+    });
 });
 
-test('withdraw more than account balance throws an error', () => {
-    account.deposit(50);
-    expect(function() {
-        account.withdraw(60)
-    }).toThrow();
-});
